408cd61c352fd3104c322d91f407d16192fb05ba
Víctor Múnera <vmunerav@icloud.com>
2026-09-20 18:08:11 +0200

    Initial Commit

---
+++ Forma3DTests/Forma3DTests.swift
@@ -0,0 +1,19 @@
+//
+//  Forma3DTests.swift
+//  Forma3DTests
+//
+//  Created by Victor Munera on 20/09/2026.
+//
+
+import Testing
+@testable import Forma3D
+
+struct Forma3DTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
